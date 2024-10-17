1ec7d69e12bb5f00f12b00a9af34c4000d84c8c3
bsaillant <bastien.saillant@liris.cnrs.fr>
2024-10-17 08:32:40 +0200

    ça va finir par marcher

--- octopus/include/Dynamic/VBD/MG_VertexBlockDescent.h
+++ octopus/include/Dynamic/VBD/MG_VertexBlockDescent.h
@@ -7,36 +7,49 @@
 #include "Dynamic/FEM/FEM_ContinuousMaterial.h"
 
 struct Grid_Level {
-    Grid_Level(FEM_Shape *shape, const Mesh::Geometry &positions, const Mesh::Topology &topology,
-               const std::vector<scalar> &masses,
-               const std::vector<std::vector<Matrix3x3> > &jx_inv, const std::vector<std::vector<scalar> > &v,
-               const Mesh::Geometry &y)
-        : _shape(shape),
-          _masses(masses),
-          _JX_inv(jx_inv),
-          _V(v),
-          _y(y), _dx(Mesh::Geometry(y.size(), Unit3D::Zero())) {
-    }
+    Grid_Level(FEM_Shape *shape,
+             const std::vector<scalar> &masses,
+             const std::vector<std::vector<Matrix3x3> > &jx_inv,
+             const std::vector<std::vector<scalar> > &v, std::vector<int> ids) :
+          _shape(shape), _masses(masses), _JX_inv(jx_inv), _V(v), _ids(ids) {}
 
     // const
     FEM_Shape *_shape; // element used in this grid level
-    std::vector<int> ids; // vertices that are used in this grid level
+    std::vector<int> _ids; // vertices that are used in this grid level
     std::vector<scalar> _masses; // for each vertice
     std::vector<std::vector<Matrix3x3> > _JX_inv; // per element
     std::vector<std::vector<scalar> > _V; // per element
-
-    // dynamic
-    Mesh::Geometry _y; // each integration
-    Mesh::Geometry _dx;
 };
 
-struct Grid_Interpolation {
-    virtual ~Grid_Interpolation() = default;
 
-    virtual void restriction(const Grid_Level *gH, Grid_Level *g2H) = 0;
 
-    virtual void prolongation(const Grid_Level *g2H, Grid_Level *gH) = 0;
+struct P1_to_P2 {
+    std::vector<int> ids;
+    std::vector<std::pair<int,int>> edges;
+    static std::pair<int,int> ref_edges[6];
+    explicit P1_to_P2(const Mesh::Topology& topology) {
+
+        for(int i = 0; i < topology.size(); i+=10) {
+            for(int j = 0; j < 6; ++j) {
+                ids.push_back(topology[i + 4 + j]);
+                const int a = ref_edges[j].first, b = ref_edges[j].second;
+                edges.emplace_back(topology[i + a], topology[i + b]);
+            }
+        }
+    }
+
+    virtual void prolongation(ParticleSystem* ps, std::vector<Vector3> dx) {
+        for(int i = 0; i < ids.size(); i++) {
+            const int a = edges[i].first, b = edges[i].second;
+            ps->get(ids[i])->position += (dx[a] + dx[b]) * 0.5f;
+        }
+
+    }
+
+    virtual ~P1_to_P2() = default;
 };
+
+std::pair<int,int> P1_to_P2::ref_edges[6] = {{0,1},{1,2},{2,0},{0,3},{1,3},{2,3}};
 
 /// Faire la technique avec 1 maillage différent pour chaque niveau (par grave pour la mémoire)
 /// Pour l'initialisation on considère que chaque niveau est une bonne approximation P1 == P2
@@ -51,22 +64,55 @@ struct Grid_Interpolation {
 /// A chaque niveau on va modifier directement p mais on va aussi retenir la correction dx
 /// à la fin de l'itération, on interpole dx vers le niveau au dessus
 struct MG_VBD_FEM {
-    MG_VBD_FEM(const Mesh::Topology &topology, ParticleSystem *ps, FEM_Shape *shape, FEM_ContinuousMaterial *material) {
-        // init global shape (P2)
+    MG_VBD_FEM(const Mesh::Topology &topology, const Mesh::Geometry &geometry, FEM_Shape *shape, FEM_ContinuousMaterial *material, scalar density) {
+        // init global data and shape (P2)
+        _shape = shape;
+        _y = geometry;
+        _material = material;
+
         // init neighboors for each particles (same for each level)
+        build_neighboors(topology);
         // init constant for P2 => init grid[0]
+        build_fem_const(topology, geometry, density, Tetra20);
         // init constant for P1 => init grid[1]
+        build_fem_const(topology, geometry, density, Tetra);
+
+        // init prolongation
+        p1_to_p2 = new P1_to_P2(topology);
     }
 
-    void build_fem_const(const Mesh::Topology &topology, ParticleSystem *ps, FEM_Shape *shape) {
-        // in : topology, density, shape type
-        // resume : init grid
-        // we suppose that P2 == P1 at rest state
+    void build_fem_const(const Mesh::Topology &topology,  const Mesh::Geometry &geometry, scalar density, Element e) {
+        const int nb_quadrature = _shape->nb_quadratures();
+        const int nb_element = static_cast<int>(topology.size()) / _shape->nb;
+        FEM_Shape* l_shape = get_fem_shape(e);
+        std::vector<std::vector<Matrix3x3> > JX_inv(nb_element);
+        std::vector<std::vector<scalar> > V(nb_element);
+        std::vector<scalar> masses(geometry.size(), 0);
+        std::set<int> s_ids;
+        for (int i = 0; i < nb_element; i++) {
+            scalar mass = 0;
+            const int id = i * _shape->nb;
 
-        // get shape
-        // build V  from shape and topo
-        // build JX from shape and topo
-        // build M  from shape and topo
+            V[i].resize(nb_quadrature);
+            JX_inv[i].resize(nb_quadrature);
+
+            for (int j = 0; j < nb_quadrature; ++j) {
+                Matrix3x3 J = Matrix::Zero3x3();
+                for (int k = 0; k < l_shape->nb; ++k) {
+                    s_ids.insert(topology[id + k]); // save all ids of this level
+                    J += glm::outerProduct(geometry[topology[id + k]], _shape->dN[j][k]);
+                }
+                V[i][j] = abs(glm::determinant(J)) * _shape->weights[j];
+                JX_inv[i][j] = glm::inverse(J);
+                mass += V[i][j];
+            }
+            mass *= density / l_shape->nb;
+            for (int k = 0; k < _shape->nb; ++k) {
+                masses[topology[id + k]] = mass;
+            }
+        }
+        std::vector<int> ids(s_ids.begin(), s_ids.end());
+        grids.push_back(new Grid_Level(l_shape, masses, JX_inv, V, ids));
     }
 
 
@@ -81,13 +127,17 @@ struct MG_VBD_FEM {
 
 
     void solve(ParticleSystem *ps, scalar dt) {
-        // Type : Cascade
-        // for each grid start at coarse
-        //      Set grid->dx to 0
-        //      for each id in grid
-        //          solve n time for grid_n to get dx
-        //      prolongation of dx to grid_n-1
-        // apply grid[0]->_dx on ps
+
+        // coarse to refined
+        for(int i = grids.size()-1; i >= 0; i++) {
+            Grid_Level *grid = grids[i];
+            std::fill(_dx.begin(), _dx.end(), Unit3D::Zero());
+            for(int id : grid->_ids) {
+                solve_vertex(ps, grid, dt, id);
+            }
+            // prolongatation
+            p1_to_p2->prolongation(ps, _dx);
+        }
     }
 
     void plot_residual(ParticleSystem *ps, scalar dt) {
@@ -102,8 +152,9 @@ struct MG_VBD_FEM {
         // can wait
     }
 
-    void solve_vertex(ParticleSystem *ps, Grid_Level* grid, scalar dt, int id) {
-        int vid = grid->ids[id];
+    // int i = vertex index for ids
+    void solve_vertex(ParticleSystem *ps, Grid_Level* grid, scalar dt, int i) {
+        int vid = grid->_ids[i];
         // need current grid info
         const int nb_owners = static_cast<int>(_owners[vid].size());
         Vector3 f_i = Unit3D::Zero();
@@ -112,17 +163,18 @@ struct MG_VBD_FEM {
         for (int i = 0; i < nb_owners; ++i) {
             const int owner = _owners[vid][i];
             const int ref_id = _ref_id[vid][i];
-            solve_element(ps, grid, id, owner,ref_id, f_i, H_i);
+            solve_element(ps, grid, owner, ref_id, f_i, H_i);
         }
         // Inertia
-        f_i += -p->mass / (dt * dt) * (p->position - grid->_y[id]);
+        f_i += -p->mass / (dt * dt) * (p->position - _y[vid]);
         H_i += Matrix3x3(p->mass / (dt * dt));
         const scalar detH = abs(glm::determinant(H_i));
         const Vector3 dx = detH > eps ? glm::inverse(H_i) * f_i : Unit3D::Zero();
-        grid->_dx[id] += dx;
+        ps->get(vid)->position += dx;
+        _dx[vid] += dx;
     }
 
-    void solve_element(ParticleSystem *ps, Grid_Level* grid, int id,  int eid, int ref_id, Vector3 &f_i, Matrix3x3 &H_i) {
+    void solve_element(ParticleSystem *ps, Grid_Level* grid, int eid, int ref_id, Vector3 &f_i, Matrix3x3 &H_i) {
         const int nb_quadrature = grid->_shape->nb_quadratures();
         const int nb_vert_elem =  grid->_shape->nb;
         const int nb_vert_elem_max =  _shape->nb;
@@ -131,7 +183,7 @@ struct MG_VBD_FEM {
             Matrix3x3 Jx = Matrix::Zero3x3();
             for (int j = 0; j < nb_vert_elem; ++j) {
                 const int vid = _topology[eid * nb_vert_elem_max + j];
-                Vector3 p = ps->get(vid)->position + grid->_dx[id]; // gros problème ici on a besoin de faire le lien
+                Vector3 p = ps->get(vid)->position; // gros problème ici on a besoin de faire le lien
                 Jx += glm::outerProduct(p, _shape->dN[i][j]);
             }
 
@@ -151,21 +203,20 @@ struct MG_VBD_FEM {
 
     void compute_inertia(ParticleSystem *ps, scalar dt) {
         // normally we should make a better approximation but osef
-        for (Grid_Level *grid: grids) {
-            for (int i = 0; i < grid->ids.size(); ++i) {
-                const int id = grid->ids[i];
-                const Particle *p = ps->get(id);
-                grid->_y[i] = p->position + p->velocity * dt + (
-                                  (p->force + p->external_forces) * p->inv_mass + Dynamic::gravity()) * dt * dt;
-            }
+
+        for (int i = 0; i < ps->nb_particles(); ++i) {
+            const Particle *p = ps->get(i);
+            _y[i] = p->position + p->velocity * dt + ((p->force + p->external_forces) * p->inv_mass + Dynamic::gravity()) * dt * dt;
         }
+
     }
 
 protected:
     std::vector<Grid_Level *> grids;
-
+    P1_to_P2* p1_to_p2;
     FEM_ContinuousMaterial *_material;
-
+    std::vector<Vector3> _y;
+    std::vector<Vector3> _dx;
     std::vector<std::vector<int> > _owners; // for each vertice
     std::vector<std::vector<int> > _ref_id; // for each vertice
     FEM_Shape *_shape;
